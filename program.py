1b3ef016c573254053c7b8b140a20498164a3c0b
Mahesh <mahesh.ey.net@gmail>
2024-05-27 23:14:38 +0530

    Added goodbye function

--- program.py
+++ program.py
@@ -3,13 +3,16 @@ def main():
     while True:
         print("\nMain Menu")
         print("1. Greet")
-        print("2. Exit")
+        print("2. Goodbye")
+        print("3. Exit")
 
         choice = input("Enter your choice: ")
 
         if choice == '1':
             greet()
         elif choice == '2':
+            goodbye()
+        elif choice == '3':
             print("Exiting...")
             break
         else:
@@ -17,6 +20,9 @@ def main():
 
 def greet():
     print("Hello, User!")
+
+def goodbye():
+    print("Goodbye, User!")
 
 if __name__ == "__main__":
     main()
